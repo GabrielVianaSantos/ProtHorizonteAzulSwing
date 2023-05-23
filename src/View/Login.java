1a8c8dd042a590da32400f99ece653699f6ad7e0
Gabriel <hhh456yt@gmail.com>
2023-05-23 01:21:01 -0300

    - Navegação entre janelas - Menu Dinamico com CardLayout - Definição das telas Registros e Pedidos no CardLayout - Botões de ações na barra de menu lateral

--- src/View/Login.java
+++ src/View/Login.java
@@ -14,35 +14,44 @@ public class Login extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         txtUsuario = new javax.swing.JTextField();
-        txtSenha = new javax.swing.JTextField();
+        txtSenha1 = new javax.swing.JTextField();
         btnLogin = new javax.swing.JButton();
         btnCancelar = new javax.swing.JButton();
         cbMostrarSenha = new javax.swing.JCheckBox();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Login");
         getContentPane().setLayout(null);
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel1.setText("Bem-Vindo de volta!");
         getContentPane().add(jLabel1);
-        jLabel1.setBounds(50, 39, 220, 25);
+        jLabel1.setBounds(40, 40, 220, 25);
 
         jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel2.setText("Login");
+        jLabel2.setText("Senha");
         getContentPane().add(jLabel2);
-        jLabel2.setBounds(50, 85, 70, 25);
+        jLabel2.setBounds(40, 170, 70, 30);
 
-        txtUsuario.setText("Usuário");
+        txtUsuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtUsuarioActionPerformed(evt);
+            }
+        });
         getContentPane().add(txtUsuario);
-        txtUsuario.setBounds(50, 119, 150, 34);
-
-        txtSenha.setText("Senha");
-        getContentPane().add(txtSenha);
-        txtSenha.setBounds(50, 171, 150, 34);
+        txtUsuario.setBounds(120, 130, 150, 30);
+        getContentPane().add(txtSenha1);
+        txtSenha1.setBounds(120, 170, 150, 30);
 
         btnLogin.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         btnLogin.setText("Login");
+        btnLogin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLoginActionPerformed(evt);
+            }
+        });
         getContentPane().add(btnLogin);
         btnLogin.setBounds(50, 261, 90, 32);
 
@@ -52,12 +61,40 @@ public class Login extends javax.swing.JFrame {
         btnCancelar.setBounds(181, 261, 110, 32);
 
         cbMostrarSenha.setText("Mostrar Senha");
+        cbMostrarSenha.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cbMostrarSenhaActionPerformed(evt);
+            }
+        });
         getContentPane().add(cbMostrarSenha);
         cbMostrarSenha.setBounds(50, 223, 99, 20);
 
-        setSize(new java.awt.Dimension(385, 346));
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel3.setText("Login");
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(40, 70, 70, 25);
+
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel4.setText("Usuário");
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(40, 130, 70, 25);
+
+        setSize(new java.awt.Dimension(385, 410));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void txtUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtUsuarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtUsuarioActionPerformed
+
+    private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
+       new Home().setVisible(true);
+       dispose();
+    }//GEN-LAST:event_btnLoginActionPerformed
+
+    private void cbMostrarSenhaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbMostrarSenhaActionPerformed
+
+    }//GEN-LAST:event_cbMostrarSenhaActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -97,7 +134,9 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JCheckBox cbMostrarSenha;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JTextField txtSenha;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JTextField txtSenha1;
     private javax.swing.JTextField txtUsuario;
     // End of variables declaration//GEN-END:variables
 }
